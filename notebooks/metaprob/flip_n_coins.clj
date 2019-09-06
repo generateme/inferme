f377f7840ab7b2257b5ec74c7704121e655c2839
generateme <generateme.blog@gmail.com>
2019-09-06 09:45:50 +0200

    model bug fixed

--- notebooks/metaprob/flip_n_coins.clj
+++ notebooks/metaprob/flip_n_coins.clj
@@ -7,7 +7,7 @@
   ([n] (flip-n-coins n []))
   ([n observations]
    (make-model
-    [tricky (:bernoulli 0.1)
+    [tricky (:bernoulli {:p 0.1})
      prior-p (:uniform-real)] ;; I have to introduce this prior to be able to set the value
     (let [p (if-not (zero? tricky) prior-p 0.5)]
       (model-result [(observe (distr :bernoulli {:p p}) observations)]
@@ -20,8 +20,8 @@
     (call (flip-n-coins n (:gen generator)))))
 
 (coin-flips-demo-n-flips 5)
-;; => {:p 0.012137751192400903, :gen (1 0 1 0 0), :prior-p 0.012137751192400903, :tricky 1.0, :LL -17.657951015300277, :LP -18.35109819586022}
-;; => {:p 0.5, :gen (1 1 0 0 0), :prior-p 0.750180949164074, :tricky 0.0, :LL -3.4657359027997265, :LP -4.1588830833596715}
+;; => {:p 0.2403560213962307, :gen (0 0 0 1 0), :prior-p 0.2403560213962307, :tricky 1.0, :LL -3.675984278671968, :LP -5.978569371666014}
+;; => {:p 0.5, :gen (1 0 1 0 0), :prior-p 0.8275730067219886, :tricky 0.0, :LL -3.4657359027997265, :LP -3.571096418457553}
 
 (def ensure-tricky-and-biased {:tricky 1
                                :prior-p 0.99})
@@ -32,5 +32,5 @@
     (call model ensure-tricky-and-biased)))
 
 (coin-flips-demo-biased 5)
-;; => {:p 0.99, :gen (0 1 1 1 1), :prior-p 0.99, :tricky 1.0, :LL -4.605170185988091, :LP -5.298317366548036}
-;; => {:p 0.99, :gen (1 1 1 1 1), :prior-p 0.99, :tricky 1.0, :LL -4.605170185988091, :LP -5.298317366548036}
+;; => {:p 0.99, :gen (1 1 1 1 1), :prior-p 0.99, :tricky 1.0, :LL -4.605170185988091, :LP -6.907755278982137}
+;; => {:p 0.99, :gen (1 0 1 1 1), :prior-p 0.99, :tricky 1.0, :LL -4.605170185988091, :LP -6.907755278982137}
